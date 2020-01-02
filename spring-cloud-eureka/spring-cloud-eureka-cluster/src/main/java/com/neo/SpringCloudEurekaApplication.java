e18e89189a1bedbf81a7fe90ffe83756d2d09bed
周俊 <zhoujun3@szcomtop.com>
2020-01-02 10:48:41 +0800

    ---

--- spring-cloud-eureka/spring-cloud-eureka-cluster/src/main/java/com/neo/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/spring-cloud-eureka-cluster/src/main/java/com/neo/SpringCloudEurekaApplication.java
@@ -3,6 +3,9 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,5 +13,14 @@ public class SpringCloudEurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			super.configure(http);
+		}
 	}
 }
